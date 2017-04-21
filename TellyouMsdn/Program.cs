ba5ba3966939d2f253a6490c2ef9f8d200d7852c
sdcb <sdflysha@qq.com>
2017-04-21 17:57:18 +0800

    complete all 3 steps.

--- TellyouMsdn/Program.cs
+++ TellyouMsdn/Program.cs
@@ -29,8 +29,8 @@ namespace TellyouMsdn
             Http.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36");
             Http.DefaultRequestHeaders.Add("Cookie", "__cfduid=dfece6ce06f3ab9d3dc691a6f82082bc41491983598; preurl=/; captchaKey=b867d8b6b8; captchaExpire=1491984191; cookietest=1; __utmt=1; __utma=86709124.2064828452.1491983600.1491983600.1491983600.1; __utmb=86709124.1.10.1491983600; __utmc=86709124; __utmz=86709124.1491983600.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); UM_distinctid=15b612709404a3-0b66107155c198-8373f6a-1fa400-15b61270941e29; CNZZDATA1605814=cnzz_eid%3D427129345-1491981796-http%253A%252F%252Fmsdn.itellyou.cn%252F%26ntime%3D1491981796; Hm_lvt_8688ca4bc18cbc647c9c68fdaef6bc24=1491807607,1491809273,1491817266,1491978000; Hm_lpvt_8688ca4bc18cbc647c9c68fdaef6bc24=1491983600");
 
-            //DoWork().Wait();
-            //CompleteItems();
+            DoWork().Wait();
+            CompleteItems();
             ExportToExcel();
         }
 
